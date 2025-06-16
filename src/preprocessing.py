12ba02d8195ca21fdbb22ad9e1db008ed747080b
Ned Stickler <nedstickler@gmail.com>
2025-06-16 22:09:00 +0100

    Add documentation to preprocessing file and initial train logic in notebook

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -6,12 +6,21 @@ from nltk.stem import WordNetLemmatizer
 
 
 def nltk_downloads() -> None:
+    """Download necessary NLTK packages."""
     nltk.download("punkt")
     nltk.download("stopwords")
     nltk.download("wordnet")
 
 
 def preprocess(text: str) -> str:
+    """Pre-process text for vectorisation and embedding.
+
+    Args:
+        text (str): Text to be cleaned.
+
+    Returns:
+        str: Cleaned text.
+    """
     text = text.lower()
     tokens = word_tokenize(text)
     stop_words = set(stopwords.words("english"))
