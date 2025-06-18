b41e3f873d820ae8a610597e1c5e08dff25f0e0d
Ned Stickler <nedstickler@gmail.com>
2025-06-18 23:59:25 +0100

    WIP: Sentence Transformers for encodings

---
+++ src/rumour_milled/encoders.py
@@ -0,0 +1,14 @@
+from sentence_transformers import SentenceTransformer
+from sklearn.base import BaseEstimator, TransformerMixin
+
+
+class SentenceTransformerEncoder(BaseEstimator):
+    def __init__(self, model_name="all-MiniLM-L6-v2"):
+        self.model_name = model_name
+        self.model = SentenceTransformer(model_name)
+
+        def fit(self, X, y=None):
+            return self
+
+        def transform(self, X):
+            return self.model.encode(X, convert_to_tensor=False)
