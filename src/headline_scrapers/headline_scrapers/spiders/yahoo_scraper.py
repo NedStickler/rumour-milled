2a4f64fa145243690fc78185b320fac961b59510
Ned Stickler <nedstickler@gmail.com>
2025-06-25 23:52:54 +0100

    Explore integrating playwright with scrapy

--- src/headline_scrapers/headline_scrapers/spiders/yahoo_scraper.py
+++ src/headline_scrapers/headline_scrapers/spiders/yahoo_scraper.py
@@ -1,15 +1,27 @@
 from pathlib import Path
-from scrapy.spiders import SitemapSpider
+from scrapy.http import HtmlResponse
+from playwright.async_api import async_playwright
 import scrapy
-import json
+import asyncio
 
 
-class BBCScraper(scrapy.Spider):
+class YahooScraper(scrapy.Spider):
     name = "yahoo"
-    start_urls = ["https://finance.yahoo.com/"]
 
-    def __init__(self):
-        self.page_counter = 0
+    def start_requests(self):
+        url = "https://finance.yahoo.com/"
+        html = asyncio.run(self.accept_cookies(url))
+        yield HtmlResponse(url=url, body=html, ecoding="utf-8")
+
+    async def accept_cookies(self, url):
+        async with async_playwright() as p:
+            browser = await p.chromium.launch()
+            page = await browser.new_page()
+            await page.goto(url)
+            await page.click("button.reject-all")
+            content = await page.content()
+            await browser.close()
+            return content
 
     def parse(self, response):
         yield {"url": response.url, "title": response.css("title::text").get()}
