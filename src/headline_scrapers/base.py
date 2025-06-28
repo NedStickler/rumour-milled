b416cbfbe9cb188c2a5b8e8d28755d60c085d42f
Ned Stickler <nedstickler@gmail.com>
2025-06-28 21:44:23 +0100

    Finish base scraper class

--- src/headline_scrapers/base.py
+++ src/headline_scrapers/base.py
@@ -1,21 +1,37 @@
 from playwright.sync_api import sync_playwright
 from queue import SimpleQueue
+from os import PathLike
+from abc import ABC, abstractmethod
+import json
 
 
 class BaseScraper:
     def __init__(
-        self, root: str, ignore_robots_txt: bool = False, max_depth: int = 3
+        self,
+        root: str,
+        locator_strings: list[str],
+        ignore_robots_txt: bool = False,
+        max_depth: int = 3,
+        save_path: PathLike = ".",
     ) -> None:
         self.root = root
+        self.locator_strings = locator_strings
         self.ignore_robots_txt = ignore_robots_txt
         self.max_depth = max_depth
+        self.save_path = save_path
         self.depth = 0
         self.queue = SimpleQueue()
         self.visited = []
         self.items = []
 
-    def start() -> None:
-        return
+    @abstractmethod
+    def start(self) -> None:
+        raise NotImplementedError("start method must be overidden by child class")
 
-    def save() -> None:
-        return
+    @abstractmethod
+    def cookies(self) -> None:
+        raise NotImplementedError("cookies method must be overidden by child class")
+
+    def save(self) -> None:
+        with open(self.save_path, "w") as f:
+            json.dump(self.items, f)
