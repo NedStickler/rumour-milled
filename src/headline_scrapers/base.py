233c874b52ca759cd83912b63ce0f6db6ed65e1c
Ned Stickler <nedstickler@gmail.com>
2025-07-08 22:04:01 +0100

    Respect robots.txt

--- src/headline_scrapers/base.py
+++ src/headline_scrapers/base.py
@@ -3,10 +3,12 @@ from queue import SimpleQueue
 from os import PathLike
 from validators.url import url
 import json
+import requests
+from urllib.robotparser import RobotFileParser
 
 
 # TODO:
-# - Point of diminishing returns?
+# - Point of diminishing return?
 
 
 class BaseScraper:
@@ -14,6 +16,7 @@ class BaseScraper:
         self,
         root: str,
         locator_strings: list[str],
+        robots_txt_url: str | None = None,
         ignore_robots_txt: bool = False,
         max_pages: int = 3,
         save_path: PathLike = ".",
@@ -32,6 +35,7 @@ class BaseScraper:
         self.save_checkpoint = save_checkpont
         self.headless = headless
         self.failures = []
+        self.robots_parser = self.setup_robots_parser(robots_txt_url)
 
     def start(self) -> None:
         with sync_playwright() as p:
@@ -46,6 +50,23 @@ class BaseScraper:
     def deal_with_cookies(self) -> None:
         return
 
+    def setup_robots_parser(self, robots_url: str | None) -> bool:
+        user_code = self.__get_status_code(robots_url)
+        root_code = self.__get_status_code(self.root + "/robots.txt")
+        if user_code < 400:
+            return RobotFileParser(robots_url)
+        if root_code < 400:
+            return RobotFileParser(self.root + "/robots.txt")
+        raise ValueError(
+            "Cannot find valid robots.txt file using robot_url or root url. Supply valid url pointing to robots.txt or set ignore_robots_txt=False"
+        )
+
+    def __get_status_code(self, url):
+        try:
+            return requests.get(url).status_code
+        except requests.exceptions.RequestException:
+            return 400
+
     def process_queue(self) -> None:
         self.queue.put(self.root)
         while not self.queue.empty() and self.page_number < self.max_pages + 1:
@@ -58,6 +79,9 @@ class BaseScraper:
                 if url(next_page) is not True:
                     continue
                 if next_page in self.visited:
+                    continue
+                if not self.robots_parser.can_fetch("*", next_page):
+                    print(f"Skipping {next_page} due to robots.txt disallow")
                     continue
 
                 print(f"({self.page_number}) Scraping {next_page}")
