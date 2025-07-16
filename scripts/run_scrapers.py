9a646500802b6182ed1731c33688405184634427
Ned Stickler <nedstickler@gmail.com>
2025-07-16 22:04:47 +0100

    Add missing / to paths

--- scripts/run_scrapers.py
+++ scripts/run_scrapers.py
@@ -12,43 +12,43 @@ if __name__ == "__main__":
 
     yahoo_scraper = YahooScraper(
         save_path=save_folder_path + "/scraped_yahoo_headlines.json",
-        config_path=configs_folder_path + "yahoo.yaml",
+        config_path=configs_folder_path + "/yahoo.yaml",
     )
     sky_scraper = SkyScraper(
         save_path=save_folder_path + "/scraped_sky_headlines.json",
-        config_path=configs_folder_path + "sky.yaml",
+        config_path=configs_folder_path + "/sky.yaml",
     )
     cbc_scraper = CBCScraper(
         save_path=save_folder_path + "/scraped_cbc_headlines.json",
-        config_path=configs_folder_path + "cbc.yaml",
+        config_path=configs_folder_path + "/cbc.yaml",
     )
     abc_scraper = ABCScraper(
         save_path=save_folder_path + "/scraped_abc_headlines.json",
-        config_path=configs_folder_path + "abc.yaml",
+        config_path=configs_folder_path + "/abc.yaml",
     )
     fox_scraper = FoxScraper(
         save_path=save_folder_path + "/scraped_fox_headlines.json",
-        config_path=configs_folder_path + "fox.yaml",
+        config_path=configs_folder_path + "/fox.yaml",
     )
     nbc_scraper = NBCScraper(
         save_path=save_folder_path + "/scraped_nbc_headlines.json",
-        config_path=configs_folder_path + "nbc.yaml",
+        config_path=configs_folder_path + "/nbc.yaml",
     )
     irish_times_scraper = IrishTimesScraper(
         save_path=save_folder_path + "/scraped_irish_times_headlines.json",
-        config_path=configs_folder_path + "irish-times.yaml",
+        config_path=configs_folder_path + "/irish-times.yaml",
     )
     businesstech_scraper = BusinessTechScraper(
         save_path=save_folder_path + "/scraped_business_tech_headlines.json",
-        config_path=configs_folder_path + "businesstech.yaml",
+        config_path=configs_folder_path + "/businesstech.yaml",
     )
     rnz_scraper = RNZScraper(
         save_path=save_folder_path + "/scraped_rnz_headlines.json",
-        config_path=configs_folder_path + "rnz.yaml",
+        config_path=configs_folder_path + "/rnz.yaml",
     )
     herald_scraper = HeraldScraper(
         save_path=save_folder_path + "/scraped_herald_headlines.json",
-        config_path=configs_folder_path + "herald.yaml",
+        config_path=configs_folder_path + "/herald.yaml",
     )
 
     scrapers = [
