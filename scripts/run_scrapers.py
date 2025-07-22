012788e80d39f3e2b70280b9d7ffc0ef252fff33
Ned Stickler <nedstickler@gmail.com>
2025-07-22 16:27:38 +0100

    Remove max_pages arg from run scraper script

--- scripts/run_scrapers.py
+++ scripts/run_scrapers.py
@@ -11,42 +11,37 @@ if __name__ == "__main__":
     configs_folder_path = "src/headline_scrapers/configs"
 
     yahoo_scraper = YahooScraper(
-        log_path=log_path,
-        config_path=configs_folder_path + "/yahoo.yaml",
-        max_pages=100,
+        log_path=log_path, config_path=configs_folder_path + "/yahoo.yaml"
     )
     sky_scraper = SkyScraper(
-        log_path=log_path, config_path=configs_folder_path + "/sky.yaml", max_pages=100
+        log_path=log_path, config_path=configs_folder_path + "/sky.yaml"
     )
     cbc_scraper = CBCScraper(
-        log_path=log_path, config_path=configs_folder_path + "/cbc.yaml", max_pages=100
+        log_path=log_path, config_path=configs_folder_path + "/cbc.yaml"
     )
     abc_scraper = ABCScraper(
-        log_path=log_path, config_path=configs_folder_path + "/abc.yaml", max_pages=100
+        log_path=log_path, config_path=configs_folder_path + "/abc.yaml"
     )
     fox_scraper = FoxScraper(
-        log_path=log_path, config_path=configs_folder_path + "/fox.yaml", max_pages=100
+        log_path=log_path, config_path=configs_folder_path + "/fox.yaml"
     )
     nbc_scraper = NBCScraper(
-        log_path=log_path, config_path=configs_folder_path + "/nbc.yaml", max_pages=100
+        log_path=log_path, config_path=configs_folder_path + "/nbc.yaml"
     )
     irish_times_scraper = IrishTimesScraper(
         log_path=log_path,
         config_path=configs_folder_path + "/irish-times.yaml",
-        max_pages=100,
     )
     businesstech_scraper = BusinessTechScraper(
         log_path=log_path,
         config_path=configs_folder_path + "/businesstech.yaml",
-        max_pages=100,
     )
     rnz_scraper = RNZScraper(
-        log_path=log_path, config_path=configs_folder_path + "/rnz.yaml", max_pages=100
+        log_path=log_path, config_path=configs_folder_path + "/rnz.yaml"
     )
     herald_scraper = HeraldScraper(
         log_path=log_path,
         config_path=configs_folder_path + "/herald.yaml",
-        max_pages=100,
     )
 
     scrapers = [
