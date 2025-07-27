d13331afd10a93c5f2ab06d27bbe9870259d9a7e
Ned Stickler <nedstickler@gmail.com>
2025-07-27 15:54:31 +0100

    Add new tokenisation and vectorisation functions to preprocessing

--- src/rumour_milled/preprocessing.py
+++ src/rumour_milled/preprocessing.py
@@ -1,8 +1,48 @@
 import nltk
+import torch
+from transformers import AutoTokenizer, AutoModel
 import pandas as pd
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
+from typing import Optional
+
+
+def tokenise_headlines(
+    headlines: list[str], model: str = "bert-base-uncased"
+) -> list[dict]:
+    tokeniser = AutoTokenizer.from_pretrained(model)
+    tokens = tokeniser(headlines, padding=True, truncation=True, return_tensors="pt")
+    return tokens
+
+
+def vectorise_tokens(
+    tokens: list[dict],
+    model: str = "bert-base-uncased",
+    batch_size: Optional[int] = None,
+) -> torch.Tensor:
+    if batch_size is None:
+        batch_size = len(tokens)
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    vectoriser = AutoModel.from_pretrained(model).to(device)
+    vectors = []
+    with torch.no_grad():
+        for i in range(0, len(tokens), batch_size):
+            print(f"Vectorising batch {i}")
+            batch_tokens = tokens[i : i + batch_size].to(device)
+            vector = vectoriser(**{k: v.to(device) for k, v in batch_tokens.items()})
+            vectors.append(vector.last_hidden_state[:, 0, :])
+    return torch.cat(vectors, dim=0)
+
+
+def tokenise_and_vectorise(
+    headlines: list[str],
+    model: str = "bert-base-uncased",
+    batch_size: Optional[int] = None,
+):
+    tokens = tokenise_headlines(headlines, model)
+    vector = vectorise_tokens(tokens, model, batch_size)
+    return vector
 
 
 def nltk_downloads() -> None:
