b5cee01b20e5332be1d33dd73ba25987c2c18e93
Ned Stickler <nedstickler@gmail.com>
2025-08-03 18:40:04 +0100

    Initial Trainer skeleton

--- src/rumour_milled/ml/train.py
+++ src/rumour_milled/ml/train.py
@@ -6,6 +6,48 @@ from torch.utils.data import TensorDataset, DataLoader
 from typing import Optional
 
 
+class Trainer:
+    def __init__(
+        self,
+        model,
+        loss_fn,
+        optimiser,
+        model_kwargs,
+        device,
+    ):
+        self.device = device or torch.device(
+            "cuda" if torch.cuda.is_available() else "cpu"
+        )
+        self.model = model(**model_kwargs).to(device)
+        self.loss_fn = loss_fn
+        self.optimiser = optimiser
+
+    def train_step(self, X, y):
+        x, y = x.to(self.device), y.to(self.device)
+        self.optimiser.zero_grad()
+        out = self.model(X)
+        loss = self.loss_fn(out, y)
+        loss.backward()
+        self.optimiser.step()
+        return loss.item()
+
+    def epoch_step(self, train_loader):
+        self.model.train()
+        total_loss = 0
+        for X, y in train_loader:
+            loss = self.train_step(X, y)
+            total_loss += loss
+        return total_loss
+
+    def train(self, train_loader, validation_loader, epochs):
+        for epoch in range(epochs):
+            train_loss = self.epoch_step(train_loader)
+            print(f"Epoch {epoch} loss: {train_loss}")
+
+    def evaluate(self):
+        return
+
+
 def train_model(
     X: torch.Tensor,
     y: torch.Tensor,
