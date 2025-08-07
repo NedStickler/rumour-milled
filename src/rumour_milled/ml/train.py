3d67d9cb040fd0215602a91604b8a5c927a18fd3
Ned Stickler <nedstickler@gmail.com>
2025-08-07 21:26:34 +0100

    Add evaluation function to trainer class

--- src/rumour_milled/ml/train.py
+++ src/rumour_milled/ml/train.py
@@ -40,8 +40,20 @@ class Trainer:
 
     def train(self, train_loader, validation_loader, epochs):
         for epoch in range(epochs):
-            loss = self.epoch_step(train_loader)
-            print(f"Epoch {epoch} loss: {loss}")
+            train_loss = self.epoch_step(train_loader)
+            output_str = f"Epoch {epoch}/{epochs} | train_loss: {train_loss}"
+            if validation_loader:
+                val_loss = self.evaluate(validation_loader)
+                output_str += f" | val_loss: {val_loss}"
+            print(output_str)
 
-    def evaluate(self):
-        return
+    def evaluate(self, validation_loader):
+        self.model.eval()
+        total_loss = 0
+        with torch.no_grad():
+            for X, y in validation_loader:
+                X, y = X.to(self.device), y.to(self.device)
+                out = self.model(X)
+                loss = self.loss_fn(out, y)
+                total_loss += loss.item()
+        return total_loss / len(validation_loader)
