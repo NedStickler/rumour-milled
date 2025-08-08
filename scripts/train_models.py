5ad0303dce33c5181dff06851a6a0e87cf3781dc
Ned Stickler <nedstickler@gmail.com>
2025-08-08 22:33:21 +0100

    Begin SageMaker model training file

--- scripts/train_models.py
+++ scripts/train_models.py
@@ -2,13 +2,20 @@ from rumour_milled.ml.models.gan import HeadlinesGenerator, HeadlinesDiscriminat
 from rumour_milled.ml.load import load_headlines
 from rumour_milled.ml.preprocess import tokenise_and_vectorise
 import torch
+from sklearn.model_selection import train_test_split
 
 
 if __name__ == "__main__":
     headlines, labels = load_headlines()
     real_headlines = [
+        headline for headline, label in zip(headlines, labels) if label == 0
+    ]
+    fake_headlines = [
         headline for headline, label in zip(headlines, labels) if label == 1
     ]
-    real_headlines_subset = real_headlines[:512]
-    X = tokenise_and_vectorise(real_headlines_subset, batch_size=128)
-    y = torch.zeros_like(X, dtype=torch.float).unsqueeze(1)
+    headlines_subset = real_headlines[:256] + fake_headlines[:256]
+    ones = torch.ones((256, 1))
+    zeroes = torch.zeros((256, 1))
+    print()
+    # X = tokenise_and_vectorise(headlines_subset, batch_size=128)
+    # y = torch.zeros_like(X, dtype=torch.float)
