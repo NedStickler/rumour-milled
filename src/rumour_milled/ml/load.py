87ec04d677f78902dfba5c7b79e44dabdf95002c
Ned Stickler <nedstickler@gmail.com>
2025-08-08 23:35:31 +0100

    Update headline loader for filtering and continue training script

--- src/rumour_milled/ml/load.py
+++ src/rumour_milled/ml/load.py
@@ -21,10 +21,20 @@ def load_external_data(
 
 
 def load_headlines(
-    filter_expression, limit: Optional[int] = None
+    filter_expression=None,
+    max_items: Optional[int] = None,
+    page_limit: Optional[int] = 512,
 ) -> tuple[list[str], list[int]]:
     hs = HeadlineStorage()
-    items = hs.get_items(filter_expression=filter_expression, limit=limit)
+
+    if filter_expression is None or max_items is None:
+        items = hs.get_all_items()
+    else:
+        items = hs.get_filtered_items(
+            filter_expression=filter_expression,
+            max_items=max_items,
+            page_limit=page_limit,
+        )
     headlines = []
     labels = []
     for headline, label in items:
