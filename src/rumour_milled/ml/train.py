06bddf816620ef6cb7b98230b42b5a0c095b73d3
Ned Stickler <nedstickler@gmail.com>
2025-08-10 23:09:48 +0100

    Successfully train simple model with correct sagemaker syntax

--- src/rumour_milled/ml/train.py
+++ src/rumour_milled/ml/train.py
@@ -17,26 +17,26 @@ class Trainer:
         self.device = device or torch.device(
             "cuda" if torch.cuda.is_available() else "cpu"
         )
-        self.model = model.to(self.device)
+        self._model = model.to(self.device)
         self.loss_fn = loss_fn
         self.optimiser = optimiser
         self.scheduler = scheduler
 
     @property
     def model(self):
-        return self.model
+        return self._model
 
     def train_batch(self, X, y):
         X, y = X.to(self.device), y.to(self.device)
         self.optimiser.zero_grad()
-        out = self.model(X)
+        out = self._model(X)
         loss = self.loss_fn(out, y)
         loss.backward()
         self.optimiser.step()
         return loss.item()
 
     def train_epoch(self, train_loader):
-        self.model.train()
+        self._model.train()
         epoch_loss = 0
         for X, y in train_loader:
             loss = self.train_batch(X, y)
@@ -58,12 +58,12 @@ class Trainer:
             print(output_str)
 
     def evaluate(self, validation_loader):
-        self.model.eval()
+        self._model.eval()
         total_loss = 0
         with torch.no_grad():
             for X, y in validation_loader:
                 X, y = X.to(self.device), y.to(self.device)
-                out = self.model(X)
+                out = self._model(X)
                 loss = self.loss_fn(out, y)
                 total_loss += loss.item()
         return total_loss / len(validation_loader)
