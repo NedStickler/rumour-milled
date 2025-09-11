71f8c40d030cf08a44840729f38a6b55cd30fcd5
Ned Stickler <nedstickler@gmail.com>
2025-09-11 22:27:19 +0100

    Add bash script to create lambda function

---
+++ scripts/push-lambda.sh
@@ -0,0 +1,13 @@
+ENVFILE=".env"
+
+if [[ -f "$ENVFILE" ]]; then
+    echo "> Loading env vars from $ENVFILE"
+    set -a
+    source "$ENVFILE"
+    set +a
+fi
+
+docker build -f services/scrape_finish_handler/Dockerfile -t rm-dev-lambda-scrape-finish-handler:latest .
+docker tag rm-dev-lambda-scrape-finish-handler:latest $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/rumour-milled/rm-dev-lambda-scrape-finish-handler:latest
+docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/rumour-milled/rm-dev-lambda-scrape-finish-handler:latest
+aws lambda create-function --function-name rm-dev-lambda-scrape-finish-handler --package-type Image --code ImageUri="$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/rumour-milled/rm-dev-lambda-scrape-finish-handler:latest" --role "arn:aws:iam::$AWS_ACCOUNT_ID:role/rm-dev-lambda-scrape-finish-handler-exec-role"
