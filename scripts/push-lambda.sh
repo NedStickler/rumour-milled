c101037cd2adededab36e2472133b8f44a38cfa6
Ned Stickler <nedstickler@gmail.com>
2025-09-12 18:47:08 +0100

    Update lambda to direct SageMaker outputs correctly

--- scripts/push-lambda.sh
+++ scripts/push-lambda.sh
@@ -7,6 +7,9 @@ if [[ -f "$ENVFILE" ]]; then
     set +a
 fi
 
+export DOCKER_DEFAULT_PLATFORM=linux/amd64
+export BUILDX_NO_DEFAULT_ATTESTATIONS=1
+
 docker build -f services/scrape_finish_handler/Dockerfile -t rm-dev-lambda-scrape-finish-handler:latest .
 docker tag rm-dev-lambda-scrape-finish-handler:latest $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/rumour-milled/rm-dev-lambda-scrape-finish-handler:latest
 docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/rumour-milled/rm-dev-lambda-scrape-finish-handler:latest
