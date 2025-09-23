b997dcf8e4a606b586d82be6bf073a78a2669fb0
Ned Stickler <nedstickler@gmail.com>
2025-09-23 14:16:01 +0100

    Test real scrape element

--- tests/scraping/test_scrape_page.py
+++ tests/scraping/test_scrape_page.py
@@ -1,6 +1,7 @@
 import asyncio
 import pytest
 from rumour_milled.scraping.base import BaseScraper
+from rumour_milled.scraping.parsers import HtmlParser
 
 
 class FakePage:
@@ -163,3 +164,23 @@ async def test_failure_propagation(monkeypatch, scraper):
         assert "https://example.com/test1" in scraper.visited
     assert scraper.queue.qsize() == 0
     assert scraper.items == []
+
+
+@pytest.mark.parametrize(
+    "html, expected_headline, expected_href",
+    [
+        (
+            """
+            <a data-ylk="ccode:ntk_assetlist_unified__en-US__news__default__default__desktop__ga__noSplit;cnt_tpc:Science;cpos:1;ct:story;elm:hdln;elmt:ct;g:4ea73421-e78a-3ac8-864a-7aef9dc92818;grpt:singlestory;itc:0;ll4:badge-science;mpos:2;p_sys:jarvis;pkgt:orphan_img;pos:1;source:strm article-river;itec:0;sec:strm;tar:yahoo.com;tar_uri:/news/science;ll3:target-modal;" class="stretched-box chromatic-ignore text-inkwell headline-m-emphasized hover:underline dark:text-grey-hair line-clamp-4 sm:line-clamp-3" href="/news/articles/ai-unlocks-rna-role-dementia-115100634.html" data-rapid_p="1" data-v9y="1">AI unlocks RNA’s role in Dementia and emerging viral threats</a>
+            """,
+            ["AI unlocks RNA’s role in Dementia and emerging viral threats"],
+            ["/news/articles/ai-unlocks-rna-role-dementia-115100634.html"],
+        )
+    ],
+)
+def test_real_element(html, expected_headline, expected_href):
+    parser = HtmlParser(html)
+    text = parser.parse_text(attrs={"class": "headline"})
+    hrefs = parser.parse_hrefs()
+    assert text == expected_headline
+    assert hrefs == expected_href
