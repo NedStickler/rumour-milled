e94fcefdc32658302e5e8542e3c71aaae72d6ce5
Ned Stickler <nedstickler@gmail.com>
2025-10-02 23:12:30 +0100

    Makefile to abstract build and teardown processes

--- src/rumour_milled/cli.py
+++ src/rumour_milled/cli.py
@@ -1,8 +1,9 @@
 import typer
 import uuid
 import pytest
+import subprocess
 from rumour_milled import log
-from rumour_milled.settings import load_settings
+from rumour_milled.settings import load_settings, Settings
 from dotenv import load_dotenv
 from typing import Literal
 
@@ -10,7 +11,7 @@ from typing import Literal
 app = typer.Typer(no_args_is_help=True, add_completion=False)
 
 
-def _boot():
+def _boot() -> Settings:
     load_dotenv()
     settings = load_settings()
     log.setup_logging(settings.log_level)
@@ -21,6 +22,10 @@ def _boot():
     return settings
 
 
+def _run(cmd: list[str]):
+    subprocess.run(cmd, check=True)
+
+
 @app.command()
 def hello(name: str) -> None:
     _boot()
@@ -28,14 +33,14 @@ def hello(name: str) -> None:
 
 
 @app.command()
-def ping():
+def ping() -> None:
     _boot()
     log.log("ping", status="ok")
 
 
 # TODO: Add module type hint as `module: Literal["all", "scraping", ...] = "all` when Typer adds support (PR was completed 19/09/2025)
 @app.command()
-def run_tests(module="all", pytest_args: list[str] = []):
+def test(module = "all", pytest_args: list[str] = []) -> None:
     _boot()
     log.log("run_tests", module=module, pytest_args=pytest_args)
 
@@ -47,6 +52,20 @@ def run_tests(module="all", pytest_args: list[str] = []):
         path = "tests/storage"
 
     pytest.main([path] + pytest_args)
+
+
+@app.command()
+def up(makefile_path: str = "infra/", env: str = "local") -> None:
+    _boot()
+    _run(["make", "-C", f"{makefile_path}", f"ENV={env}", "all"])
+    log.log("up", env=env)
+
+
+@app.command()
+def down(makefile_path: str = "infra/") -> None:
+    _boot()
+    _run(["make", "-C", f"{makefile_path}", "down"])
+    log.log(down)
 
 
 if __name__ == "__main__":
